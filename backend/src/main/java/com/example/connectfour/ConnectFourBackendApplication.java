20d47997a45dec353abbc17386f10c10c2ece9de
Brendon Lam <jeremycheungsubacc1995@outlook.com>
2026-08-16 23:01:22 +1000

    build(backend): scaffold Spring Boot app

---
+++ backend/src/main/java/com/example/connectfour/ConnectFourBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFourBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFourBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/connectfour/ConnectFourBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connectfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectFourBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
